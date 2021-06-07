ec781044409e7e28a25d353b9f3289822ad07f30
Emeline <leduc.emeline44@gmail.com>
2021-06-07 17:40:59 +0200

    add implementation

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -3,7 +3,10 @@ const assert = require("assert");
 
 // WRITE THE ACTUAL FUNCTION HERE
 function capitalizeFirst(input) {
-  return input.length > 0 ? input[0].toUpperCase() + input.slice(1) : "";
+  return input
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
 
 // Check that capitalizeFirst is a function
@@ -16,7 +19,7 @@ assert.strictEqual(capitalizeFirst.length, 1);
 assert.strictEqual(capitalizeFirst("i am learning TDD"), "I Am Learning TDD");
 
 // Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirst("z"), "Z");
+assert.strictEqual(capitalizeFirst("learning"), "Learning");
 
 // Check that it works for an empty string
 assert.strictEqual(capitalizeFirst(""), "");
